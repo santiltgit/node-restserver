61140793b22158dd5d93a1d3382c27a3b1b782af
santiltgit <santiagoloaizataborda@gmail.com>
2025-07-13 19:27:11 -0500

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,62 @@
+const { response } = require('express');
+
+
+const usuariosGet = (req, res = response) => {
+
+    const { q, nombre = 'No name', apikey, page =1, limit } = req.query; // Para obtener los parámetros de la URL
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - usuariosPost',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - usuariosPut',
+        id 
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+
+
+    res.json({
+        msg: 'patch API - usuariosPatch'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+
+
+    res.json({
+        msg: 'delete API - usuariosDelete'
+    });
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+};
